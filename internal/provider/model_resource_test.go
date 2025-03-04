bd1daa8c8fe9a5abba1801d7a310e5af80a1b679
Mona LisaMona LisaMona LisaMona LisaMona LisaMona LisaMona Lisa <shilpa@etleap.com>
2025-03-04 16:28:22 +0000

    Randomising model table name

--- internal/provider/model_resource_test.go
+++ internal/provider/model_resource_test.go
@@ -32,7 +32,7 @@ resource "etleap_model" "my_model" {
 		}
 		materialized_view           = true
 		schema                      = "public"
-		table                       = "tf_test_table"
+		table                       = "<<.TableName>>"
 		type                        = "REDSHIFT"
 		wait_for_update_preparation = false
 	  }
@@ -45,11 +45,13 @@ var modelTemplate, _ = template.New("ModelTemplate").Delims(`<<`, `>>`).Parse(MO
 
 type ModelConfig struct {
 	Name string
+	TableName string
 	testutils.TestConstants
 }
 
 func TestAccModel(t *testing.T) {
 	modelName := acctest.RandomWithPrefix("Model")
+    tableName := acctest.RandomWithPrefix("tf_test_table")
 
 	resource.ParallelTest(t, resource.TestCase{
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
@@ -57,6 +59,7 @@ func TestAccModel(t *testing.T) {
 			{ // Resource creation
 				Config: GetProviderDefinition() + getModelConfig(&ModelConfig{
 					modelName,
+					tableName,
 					*testutils.Constants,
 				}),
 				ConfigPlanChecks: resource.ConfigPlanChecks{
@@ -72,6 +75,7 @@ func TestAccModel(t *testing.T) {
 			{ // Resource Modification
 				Config: GetProviderDefinition() + getModelConfig(&ModelConfig{
 					modelName + " 2",
+					tableName,
 					*testutils.Constants,
 				}),
 				ConfigPlanChecks: resource.ConfigPlanChecks{
