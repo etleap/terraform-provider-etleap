6486f8bca6adeddc0fe48e099e4a98a5f0423fdc
Etleap Dev <dev@etleap.com>
2026-05-11 18:56:08 +0000

    Regenerated provider and docs

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     etleap = {
       source  = "etleap/etleap"
-      version = "0.3.5"
+      version = "0.3.6"
     }
   }
 }
--- internal/sdk/sdk.go
+++ internal/sdk/sdk.go
@@ -69,6 +69,7 @@ type SDK struct {
 	Team        *Team
 	User        *User
 	DbtSchedule *DbtSchedule
+	DbtSettings *DbtSettings
 
 	sdkConfiguration sdkConfiguration
 }
@@ -144,10 +145,10 @@ func New(opts ...SDKOption) *SDK {
 	sdk := &SDK{
 		sdkConfiguration: sdkConfiguration{
 			Language:          "go",
-			OpenAPIDocVersion: "2.9.5",
-			SDKVersion:        "0.3.5",
+			OpenAPIDocVersion: "2.9.6",
+			SDKVersion:        "0.3.6",
 			GenVersion:        "2.248.6",
-			UserAgent:         "speakeasy-sdk/go 0.3.5 2.248.6 2.9.5 etleap",
+			UserAgent:         "speakeasy-sdk/go 0.3.6 2.248.6 2.9.6 etleap",
 		},
 	}
 	for _, opt := range opts {
@@ -177,6 +178,8 @@ func New(opts ...SDKOption) *SDK {
 	sdk.User = newUser(sdk.sdkConfiguration)
 
 	sdk.DbtSchedule = newDbtSchedule(sdk.sdkConfiguration)
+
+	sdk.DbtSettings = newDbtSettings(sdk.sdkConfiguration)
 
 	return sdk
 }
