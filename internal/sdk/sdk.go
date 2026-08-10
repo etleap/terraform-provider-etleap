1eb9cabf685a16f449240b94cf024e0a92a89c5b
Etleap Dev <dev@etleap.com>
2026-08-10 14:08:03 +0000

    Regenerated provider and docs

--- internal/sdk/sdk.go
+++ internal/sdk/sdk.go
@@ -145,10 +145,10 @@ func New(opts ...SDKOption) *SDK {
 	sdk := &SDK{
 		sdkConfiguration: sdkConfiguration{
 			Language:          "go",
-			OpenAPIDocVersion: "2.9.19",
-			SDKVersion:        "0.3.17",
+			OpenAPIDocVersion: "2.9.18",
+			SDKVersion:        "0.3.18",
 			GenVersion:        "2.248.6",
-			UserAgent:         "speakeasy-sdk/go 0.3.17 2.248.6 2.9.19 etleap",
+			UserAgent:         "speakeasy-sdk/go 0.3.18 2.248.6 2.9.18 etleap",
 		},
 	}
 	for _, opt := range opts {
